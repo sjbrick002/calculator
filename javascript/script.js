be4aecce98e42917b2fa862d867dcfec085d263a
SBrick <samuelbrick3@gmail.com>
2021-11-06 18:04:45 -0500

    Created calculator gui and basic operator functions

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+function sum(num1, num2) {
+    return num1 + num2;
+};
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+function operate(num1, num2, operation) {
+    return operation(num1, num2);
+} 
