3485ed3af1fd543156eda38b0cb6c19a41989f23
SBrick <samuelbrick3@gmail.com>
2021-11-14 13:57:47 -0600

    Created functional decimal button and functional negative button

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,14 @@
 function sum(num1, num2) {
-    return num1 + num2;
+    return Math.round((num1 + num2) * 10000000000) / 10000000000;
 };
 function difference(num1, num2) {
-    return num1 - num2;
+    return Math.round((num1 - num2) * 10000000000) / 10000000000;
 };
 function product(num1, num2) {
-    return num1 * num2;
+    return Math.round((num1 * num2) * 10000000000) / 10000000000;
 };
 function quotient(num1, num2) {
-    return num1 / num2;
+    return Math.round((num1 / num2) * 10000000000) / 10000000000;
 };
 
 function operate(num1, num2, operation) {
@@ -23,74 +23,66 @@ let answer;
 
 
 //display = document.querySelector(".display")
-firstValue = document.querySelector(".first-value");
-operatorValue = document.querySelector(".operator-value");
-secondValue = document.querySelector(".second-value");
+const firstValue = document.querySelector(".first-value");
+firstValue.textContent = 0;
+const operatorValue = document.querySelector(".operator-value");
+const secondValue = document.querySelector(".second-value");
 
-clearBtn = document.querySelector(".clear");
+const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", () => {
     firstValue.textContent = "0";
-    operatorValue.textContent = "";
-    secondValue.textContent = "";
+    operatorValue.textContent = null;
+    secondValue.textContent = null;
     firstNumber = null;
     secondNumber = null;
     answer = null;
     operator = null;
+    hasDecimalOne = false;
+    hasDecimalTwo = false;
+    firstNegative = false;
+    secondNegative = false;
 });
 
-divisionBtn = document.querySelector(".division");
+const divisionBtn = document.querySelector(".division");
 divisionBtn.addEventListener("click", () => {
-    firstNumber = Number(firstValue.textContent);
     if (secondValue.textContent) {
-
-        secondNumber = Number(secondValue.textContent);
-        answer = operate(firstNumber, secondNumber, operator);
-
-        firstValue.textContent = answer;
-        secondValue.textContent = "";
+        getAnswer();
+        hasDecimalTwo = false;
     };
-    operatorValue.textContent = "/";
+    if (firstValue.textContent !== "TRY AGAIN!!!") {
+        operatorValue.textContent = "/";
+    };
     operator = quotient;
 });
 
-multiplicationBtn = document.querySelector(".multiplication");
+const multiplicationBtn = document.querySelector(".multiplication");
 multiplicationBtn.addEventListener("click", () => {
-    firstNumber = Number(firstValue.textContent);
     if (secondValue.textContent) {
-
-        secondNumber = Number(secondValue.textContent);
-        answer = operate(firstNumber, secondNumber, operator);
-
-        firstValue.textContent = answer;
-        secondValue.textContent = "";
+        getAnswer();
+        hasDecimalTwo = false;
     };
     operatorValue.textContent = "*";
     operator = product;
 });
 
-
-subtractionBtn = document.querySelector(".subtraction");
+const subtractionBtn = document.querySelector(".subtraction");
 subtractionBtn.addEventListener("click", () => {
-    firstNumber = Number(firstValue.textContent);
     if (secondValue.textContent) {
-
-        secondNumber = Number(secondValue.textContent);
-        answer = operate(firstNumber, secondNumber, operator);
-
-        firstValue.textContent = answer;
-        secondValue.textContent = "";
+        getAnswer();
+        hasDecimalTwo = false;
     };
     operatorValue.textContent = "-";
     operator = difference;
 });
 
-
-sevenBtn = document.querySelector(".seven");
+const sevenBtn = document.querySelector(".seven");
 sevenBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 7;
-        } else {
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -7;
+        }else {
             firstValue.textContent += 7;
         };
     } else {
@@ -98,11 +90,13 @@ sevenBtn.addEventListener("click", () => {
     };
 });
 
-eightBtn = document.querySelector(".eight");
+const eightBtn = document.querySelector(".eight");
 eightBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 8;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -8;
         } else {
             firstValue.textContent += 8;
         };
@@ -111,11 +105,13 @@ eightBtn.addEventListener("click", () => {
     };
 });
 
-nineBtn = document.querySelector(".nine");
+const nineBtn = document.querySelector(".nine");
 nineBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 9;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -9;
         } else {
             firstValue.textContent += 9;
         };
@@ -124,11 +120,13 @@ nineBtn.addEventListener("click", () => {
     };
 });
 
-fourBtn = document.querySelector(".four");
+const fourBtn = document.querySelector(".four");
 fourBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 4;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -4;
         } else {
             firstValue.textContent += 4;
         };
@@ -137,11 +135,13 @@ fourBtn.addEventListener("click", () => {
     };
 });
 
-fiveBtn = document.querySelector(".five");
+const fiveBtn = document.querySelector(".five");
 fiveBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 5;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -5;
         } else {
             firstValue.textContent += 5;
         };
@@ -150,11 +150,13 @@ fiveBtn.addEventListener("click", () => {
     };
 });
 
-sixBtn = document.querySelector(".six");
+const sixBtn = document.querySelector(".six");
 sixBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 6;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -6;
         } else {
             firstValue.textContent += 6;
         };
@@ -163,27 +165,24 @@ sixBtn.addEventListener("click", () => {
     };
 });
 
-additionBtn = document.querySelector(".addition");
+const additionBtn = document.querySelector(".addition");
 additionBtn.addEventListener("click", () => {
-    firstNumber = Number(firstValue.textContent);
     if (secondValue.textContent) {
-
-        secondNumber = Number(secondValue.textContent);
-        answer = operate(firstNumber, secondNumber, operator);
-
-        firstValue.textContent = answer;
-        secondValue.textContent = "";
+        getAnswer();
+        hasDecimalTwo = false;
     };
     operatorValue.textContent = "+";
     operator = sum;
 });
 
 
-oneBtn = document.querySelector(".one");
+const oneBtn = document.querySelector(".one");
 oneBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 1;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -1;
         } else {
             firstValue.textContent += 1;
         };
@@ -192,11 +191,13 @@ oneBtn.addEventListener("click", () => {
     };
 });
 
-twoBtn = document.querySelector(".two");
+const twoBtn = document.querySelector(".two");
 twoBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 2;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -2;
         } else {
             firstValue.textContent += 2;
         };
@@ -205,11 +206,13 @@ twoBtn.addEventListener("click", () => {
     };
 });
 
-threeBtn = document.querySelector(".three");
+const threeBtn = document.querySelector(".three");
 threeBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 3;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -3;
         } else {
             firstValue.textContent += 3;
         };
@@ -218,11 +221,13 @@ threeBtn.addEventListener("click", () => {
     };
 });
 
-zeroBtn = document.querySelector(".zero");
+const zeroBtn = document.querySelector(".zero");
 zeroBtn.addEventListener("click", () => {
     if (operator === null) {
-        if (firstValue.textContent === "0") {
+        if (firstValue.textContent === "0" || firstValue.textContent == answer) {
             firstValue.textContent = 0;
+        } else if (firstValue.textContent === "-0") {
+            firstValue.textContent = -0;
         } else {
             firstValue.textContent += 0;
         };
@@ -231,17 +236,75 @@ zeroBtn.addEventListener("click", () => {
     };
 });
 
-decimalBtn = document.querySelector(".decimal");
-//decimalBtn.addEventListener("click", () => display.textContent += ".");
+let hasDecimalOne = false;
+let hasDecimalTwo = false;
+
+const decimalBtn = document.querySelector(".decimal");
+decimalBtn.addEventListener("click", () => {
+    if (secondValue.textContent && hasDecimalTwo === false) {
+        secondValue.textContent += ".";
+        hasDecimalTwo = true;
+    } else if (hasDecimalOne === false) {
+        firstValue.textContent += ".";
+        hasDecimalOne = true;
+    }
+});
+
+
+let firstNegative = false;
+let secondNegative = false;
+
+const negativeBtn = document.querySelector(".negative");
+negativeBtn.addEventListener("click", () => {
+    if (operatorValue.textContent) {
+        if (secondNegative === false) {
+            let secondValueArr = secondValue.textContent.split("");
+            console.log(secondValueArr);
+            secondValueArr.unshift("-");
+            console.log(secondValueArr);
+            secondValue.textContent = secondValueArr.join("");
+            secondNegative = true;
+        } else {
+            let secondValueArr = secondValue.textContent.split("");
+            console.log(secondValueArr);
+            secondValueArr.shift();
+            console.log(secondValueArr);
+            secondValue.textContent = secondValueArr.join("");
+            secondNegative = false;
+        }
+    } else {
+        if (firstNegative === false) {
+            let firstValueArr = firstValue.textContent.split("");
+            firstValueArr.unshift("-");
+            firstValue.textContent = firstValueArr.join("");
+            firstNegative = true;
+        } else {
+            let firstValueArr = firstValue.textContent.split("");
+            firstValueArr.shift();
+            firstValue.textContent = firstValueArr.join("");
+            firstNegative = false;
+        }
+    }
+});
 
 operateBtn = document.querySelector(".operate-btn");
 operateBtn.addEventListener("click", () => {
+    getAnswer();
+    hasDecimalOne = false;
+    hasDecimalTwo = false;
+});
+
+function getAnswer() {
     firstNumber = Number(firstValue.textContent);
     secondNumber = Number(secondValue.textContent);
-    answer = operate(firstNumber, secondNumber, operator);
+    if (secondNumber === 0 && operator === quotient) {
+        answer = "U MAD LAD";
+    } else {
+        answer = operate(firstNumber, secondNumber, operator);
+    }
     operator = null;
 
     firstValue.textContent = answer;
     operatorValue.textContent = "";
     secondValue.textContent = "";
-});
+};
